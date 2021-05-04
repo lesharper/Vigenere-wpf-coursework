cb5feda77b435a39cdc64410843dbe788ceadc32
Aleksey-devback <bolshoy.enot@mail.ru>
2021-05-04 19:47:21 +0800

    first commit

---
+++ Secret/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Secret
+{
+    public partial class MainWindow : Window
+    {
+        public bool isEncrypt;
+        public bool isDecrypt;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rdbIsEncrypt_Checked(object sender, RoutedEventArgs e)
+        {
+            isEncrypt = true;
+            isDecrypt = false;
+        }
+
+        private void rdbIsDecrypt_Checked(object sender, RoutedEventArgs e)
+        {
+            isDecrypt = true;
+            isEncrypt = false;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            var cipher = new VigenerCipher();
+
+            if ((txtInitial != null) && (txtKey != null) && isEncrypt)
+            {
+                var encryptedText = cipher.Encrypt(txtInitial.Text, txtKey.Text);
+                txtChange.Text = encryptedText;
+            }
+            else if ((txtInitial != null) && (txtKey != null) && isDecrypt)
+            {
+                var decryptText = cipher.Decrypt(txtInitial.Text, txtKey.Text);
+                txtChange.Text = decryptText;
+            }
+
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtChange.Text);
+        }
+    }
+}
---
+++ Secret/VigenerCipher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secret
+{
+    public class VigenerCipher
+    {
+        const string defaultAlphabet = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯabcdefghiklmnopqrstuwxyzабвгдеёжзийклмнопрстуфхцшщъыьэюя*.,-_/'|1234567890";
+        readonly string letters;
+
+        public VigenerCipher(string alphabet = null)
+        {
+            letters = string.IsNullOrEmpty(alphabet) ? defaultAlphabet : alphabet;
+        }
+
+        //Генерация повторяющегося пароля
+        private string GetRepeatKey(string s, int n)
+        {
+            var p = s;
+            while (p.Length < n)
+            {
+                p += p;
+            }
+            return p.Substring(0, n);
+        }
+
+        private string Vigenere(string text, string password, bool encrypting = true)
+        {
+            var gamma = GetRepeatKey(password, text.Length);
+            var retValue = String.Empty;
+            var q = letters.Length;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var letterIndex = letters.IndexOf(text[i]);
+                var codeIndex = letters.IndexOf(gamma[i]);
+                if (letterIndex < 0)
+                {
+                    //Если буква не найдена, добавляем её в исходном виде
+                    retValue += text[i].ToString();
+                }
+                else
+                {
+                    retValue += letters[(q + letterIndex + ((encrypting ? 1 : -1) * codeIndex)) % q].ToString();
+                }
+            }
+
+            return retValue;
+        }
+
+        //Шифрование
+        public string Encrypt(string plainMessage, string password)
+        => Vigenere(plainMessage, password);
+
+        //Расшифрование
+        public string Decrypt(string encryptedMessage, string password)
+        => Vigenere(encryptedMessage, password, false);
+    }
+}
